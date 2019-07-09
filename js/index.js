354fceba6a7728fe8c0f9c71a8277a052b692b2a
Blake <bpro0821@gmail.com>
2019-07-09 14:21:46 -0500

    added wheel event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DBLCLK
+let wordIndex = 0
+
+
+ const introParagraph = document.querySelector('.home p')
+const introParagraphContent = introParagraph.innerHTML
+introParagraph.addEventListener('wheel', () => {
+  introParagraph.innerHTML = introParagraphContent
+    .split(' ')
+    .map(
+      (element, i) =>
+        i === wordIndex ? `<span class='red-word'>${element}</span>` : element
+    )
+    .join(' ')
+  document.querySelector('.home p .red-word').style.fontWeight = 'bold'
+  document.querySelector('.home p .red-word').style.fontSize = '2.4rem'
+  wordIndex =
+    wordIndex === introParagraphContent.split(' ').length - 1
+      ? 0
+      : wordIndex + 1
+})
